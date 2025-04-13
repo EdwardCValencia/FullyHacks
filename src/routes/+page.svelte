47a9674efb2b1d116bd09d32903323c48f981d05
EdwardCV <evalencia7@csu.fullerton.edu>
2025-04-13 00:03:55 -0700

    update

---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<svelte:head>
+    <title>Alien Encounters</title>
+</svelte:head>
+
+<main>
+    <p>Alien Communication</p>
+
+    <div id="frame">
+
+    </div>
+</main>
+
+<style>
+    * {
+        font-family: 'Courier New', Courier, monospace;
+        font-weight: 600;
+        font-size: large;
+    }
+    main {
+        align-items: center;
+        justify-content: center;
+    }
+    p {
+        text-align: center;
+    }
+    
+    #frame {
+        width: 80rem;
+        height: 55rem;
+        background-color: black;
+        margin:auto;
+    }
+    
+</style>
